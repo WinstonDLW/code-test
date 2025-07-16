4bd64242e5b126d74db8c3478334d4631f0f1c8e
WinstonDLW <winston.wdl92@gmail.com>
2025-07-16 13:00:33 +0930

    Refactor ToCommaSeparatedList to improve readability

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -15,25 +15,26 @@ namespace PointsBet_Backend_Online_Code_Test
 
     //If we don't have to use StringBuilder
     //We can use the combination of string.Join and LinQ: string.Join(",", items.Select(x => "the format we want"))
+    //Then loop can be saved for further simplifying
     public class StringFormatter
     {
         public static string ToCommaSeparatedList(string[] items, string quote)
         {
             if (items == null || items.Length == 0)
             {
-                return "";
+                return string.Empty;
             }
 
             StringBuilder qry = new StringBuilder();
 
-            for (int i = 0; i < items.Length; i++)
+            foreach (var item in items)
             {
                 if (qry.Length > 0)
                 {
                     qry.Append(", ");
                 }
 
-                qry.AppendFormat("{0}{1}{0}", quote, items[i]);
+                qry.AppendFormat("{0}{1}{0}", quote, item);
             }
 
             return qry.ToString();
